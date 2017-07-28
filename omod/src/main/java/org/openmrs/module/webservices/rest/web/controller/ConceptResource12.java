8b62519ce640308d1c1972a0442f691029b1587e
Andrew <andrewndungu@gmail.com>
2017-07-28 14:50:07 +0300

    Add an extra 'value' field on the concept response object

--- omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/ConceptResource12.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/ConceptResource12.java
@@ -1,24 +1,26 @@
 package org.openmrs.module.webservices.rest.web.controller;
 
-import org.openmrs.ConceptName;
-import org.openmrs.module.webservices.rest.web.annotation.SubResource;
-import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.Concept;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
-import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9.ConceptNameResource1_9;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9.ConceptResource1_9;
 
 /**
- * ConceptName Resource. Include 'tags' property
+ * Concept Resource. Add 'value' property
  */
-@SubResource(parent = ConceptResource1_9.class, path = "name", supportedClass = ConceptName.class,
-        supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*" }, order = 1)
-public class ConceptNameResource11 extends ConceptNameResource1_9 {
+@Resource(name = RestConstants.VERSION_1 + "/concept", order = 1, supportedClass = Concept.class,
+        supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*" })
+public class ConceptResource12 extends ConceptResource1_9 {
 
 	@Override
-	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
-		DelegatingResourceDescription delegatingResourceDescription = super.getRepresentationDescription(rep);
-		delegatingResourceDescription.addProperty("tags", Representation.FULL);
+	protected DelegatingResourceDescription fullRepresentationDescription(Concept delegate) {
+		DelegatingResourceDescription description = super.fullRepresentationDescription(delegate);
+		description.addProperty("value", findMethod("getConceptNameValue"));
+		return description;
+	}
 
-		return delegatingResourceDescription;
+	public String getConceptNameValue(Concept concept) {
+		return "ConceptName:" + concept.getName().getId();
 	}
 }
