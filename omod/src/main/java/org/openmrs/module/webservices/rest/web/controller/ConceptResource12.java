c7ff8bd0e96e46cb1088dc29c8072eb782d59334
Andrew <andrewndungu@gmail.com>
2017-08-01 14:07:39 +0300

    Return required fields

--- omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/ConceptResource12.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/ConceptResource12.java
@@ -3,6 +3,7 @@ package org.openmrs.module.webservices.rest.web.controller;
 import org.openmrs.Concept;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9.ConceptResource1_9;
 
@@ -14,9 +15,16 @@ import org.openmrs.module.webservices.rest.web.v1_0.resource.openmrs1_9.ConceptR
 public class ConceptResource12 extends ConceptResource1_9 {
 
 	@Override
-	protected DelegatingResourceDescription fullRepresentationDescription(Concept delegate) {
-		DelegatingResourceDescription description = super.fullRepresentationDescription(delegate);
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+		DelegatingResourceDescription description = new DelegatingResourceDescription();
+		description.addProperty("uuid");
 		description.addProperty("value", findMethod("getConceptNameValue"));
+		description.addProperty("display", findMethod("getDisplayName"));
+		description.addProperty("name", Representation.DEFAULT);
+		description.addProperty("datatype", Representation.REF);
+		description.addProperty("conceptClass", Representation.REF);
+		description.addProperty("mappings", Representation.REF);
+
 		return description;
 	}
 
