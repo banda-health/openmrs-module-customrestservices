2c05a2952e7f28c1f5e2722ac642f2d598cc65c0
Andrew <andrewndungu@gmail.com>
2017-09-01 17:36:06 +0300

    CRS-2 Patient Summary Info Merging (change merge library)

--- omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/VisitNoteResourceController.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/web/controller/VisitNoteResourceController.java
@@ -1,11 +1,14 @@
 package org.openmrs.module.webservices.rest.web.controller;
 
-import com.sksamuel.diffpatch.DiffMatchPatch;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.eclipse.jgit.diff.EditList;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.diff.HistogramDiff;
+import org.eclipse.jgit.diff.RawText;
+import org.eclipse.jgit.diff.RawTextComparator;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
@@ -30,7 +33,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.LinkedList;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Save & Update Visit Note.
@@ -39,7 +44,10 @@ import java.util.LinkedList;
 @RequestMapping("/rest/" + ModuleRestConstants.VISIT_NOTE_RESOURCE)
 public class VisitNoteResourceController {
 
+	private final SimpleDateFormat patientSummaryDateFormat =
+	        new SimpleDateFormat("dd/MM/yyyy hh:mm");
 	private final Log LOG = LogFactory.getLog(this.getClass());
+	private static final String NO_NEWLINE_AT_END_OF_FILE = "\\ No newline at end of file";
 
 	@Autowired
 	private FeatureToggleProperties featureToggles;
@@ -87,52 +95,9 @@ public class VisitNoteResourceController {
 		}
 	}
 
-	private SimpleObject mergePatientSummaryInfo(Obs updatedObs, Obs existingObs, HttpServletRequest request) {
-		String existingPatientSummary = existingObs.getValueText();
-		if (existingPatientSummary.equalsIgnoreCase("")) {
-			// no need for merging
-			existingObs.setVoided(true);
-		} else {
-			String updatedPatientSummary = request.getParameter("w12");
-			DiffMatchPatch matchPatch = new DiffMatchPatch();
-			LinkedList<DiffMatchPatch.Diff> diffs = matchPatch.diff_main(
-			    existingPatientSummary, updatedPatientSummary);
-			StringBuilder mergedText = new StringBuilder();
-			for (DiffMatchPatch.Diff diff : diffs) {
-				switch (diff.operation) {
-					case EQUAL:
-						mergedText.append(diff.text);
-						break;
-					case DELETE:
-						mergedText.append("[" + diff.text + "]");
-						break;
-					case INSERT:
-						mergedText.append(diff.text);
-						break;
-					default:
-						break;
-				}
-			}
-
-			updatedObs.setValueText(mergedText.toString());
-		}
-
-		obsService.voidObs(existingObs, "void patient summary obs");
-		Obs mergedObs = Obs.newInstance(updatedObs);
-		mergedObs.setVoided(false);
-		obsService.saveObs(mergedObs, "create merged patient summary obs");
-
-		return SimpleObject.create(
-		    "success", true,
-		    "encounterId", mergedObs.getEncounter().getUuid(),
-		    "w12", mergedObs.getValueText());
-	}
-
 	private SimpleObject saveVisitNote(Patient patient, Encounter encounter,
 	        Visit visit, Boolean createVisit,
 	        String returnUrl, HttpServletRequest request) {
-
-		SimpleObject result = new SimpleObject();
 
 		HtmlForm hf = null;
 
@@ -159,15 +124,53 @@ public class VisitNoteResourceController {
 		}
 
 		Visit updatedVisit = Context.getVisitService().getVisitByUuid(visit.getUuid());
+		Obs createdObs = null;
 		for (Encounter updatedEncounter : updatedVisit.getEncounters()) {
 			String encounterTypeName = updatedEncounter.getEncounterType().getName();
 			if (encounterTypeName.equalsIgnoreCase("Visit Note")) {
-				result = SimpleObject.create("success", true, "encounterId", updatedEncounter.getUuid());
-				break;
+				for (Obs obs : encounter.getAllObs()) {
+					if (obs.getConcept().getName().getName().equalsIgnoreCase("text of encounter note")) {
+						createdObs = obs;
+						break;
+					}
+				}
 			}
 		}
 
-		return result;
+		return SimpleObject.create(
+		    "success", true,
+		    "encounterId", createdObs.getEncounter().getUuid(),
+		    "w12", createdObs.getValueText(),
+		    "observationUuid", createdObs.getUuid());
+	}
+
+	/**
+	 * This method compares between the current and previous obs patient summary (clinical note) merges text
+	 * @param updatedObs
+	 * @param existingObs
+	 * @param request
+	 * @return
+	 */
+	private SimpleObject mergePatientSummaryInfo(Obs updatedObs, Obs existingObs, HttpServletRequest request) {
+		String existingPatientSummary = existingObs.getValueText();
+		if (existingPatientSummary.equalsIgnoreCase("")) {
+			// no need for merging
+			existingObs.setVoided(true);
+		} else {
+			updatedObs.setValueText(
+			        mergeTextAndShowConflicts(existingObs, updatedObs, request));
+		}
+
+		obsService.voidObs(existingObs, "void patient summary obs");
+		Obs mergedObs = Obs.newInstance(updatedObs);
+		mergedObs.setVoided(false);
+		Obs createdObs = obsService.saveObs(mergedObs, "create merged patient summary obs");
+
+		return SimpleObject.create(
+		    "success", true,
+		    "encounterId", createdObs.getEncounter().getUuid(),
+		    "w12", createdObs.getValueText(),
+		    "observationUuid", createdObs.getUuid());
 	}
 
 	private Obs retrieveExistingObs(Obs updatedObs, Encounter encounter) {
@@ -178,5 +181,57 @@ public class VisitNoteResourceController {
 		}
 
 		return null;
+	}
+
+	private String mergeTextAndShowConflicts(Obs existingObs, Obs updatedObs, HttpServletRequest request) {
+		StringBuilder mergedText = new StringBuilder();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			RawText rawText1 = new RawText(existingObs.getValueText().getBytes());
+			RawText rawText2 = new RawText(request.getParameter("w12").getBytes());
+			EditList diffList = new EditList();
+			diffList.addAll(new HistogramDiff().diff(RawTextComparator.DEFAULT, rawText1, rawText2));
+			new DiffFormatter(out).format(diffList, rawText1, rawText2);
+		} catch (IOException ex) {
+			out = null;
+		}
+
+		String createdBy = existingObs.getCreator().getGivenName();
+		String createdOn = patientSummaryDateFormat.format(existingObs.getDateCreated());
+		String updatedBy = updatedObs.getCreator().getGivenName();
+		String updatedOn = patientSummaryDateFormat.format(new Date());
+
+		String[] mergedTxts = StringUtils.split(out.toString(), "\n");
+		if (mergedTxts.length > 1) {
+			mergedTxts[0] = "";
+			mergedText.append(insertMetadata(createdBy, createdOn));
+			boolean firstOccurenceNewLine = false;
+			for (String text : mergedTxts) {
+				// replace first occurrence of NO_NEWLINE_AT_END_OF_FILE with updated Metadata.
+				if (!firstOccurenceNewLine && text.contains(NO_NEWLINE_AT_END_OF_FILE)) {
+					text = text.
+					        replaceFirst(NO_NEWLINE_AT_END_OF_FILE, insertMetadata(updatedBy, updatedOn));
+					firstOccurenceNewLine = true;
+				}
+
+				mergedText.append(text
+				        .replaceAll(NO_NEWLINE_AT_END_OF_FILE, "")
+				        .replaceAll("\\\\", "")
+				        .replaceAll("\\+", "")
+				        .replaceAll("-", ""));
+			}
+		}
+
+		return mergedText.toString();
+	}
+
+	private String insertMetadata(String author, String changedOn) {
+		StringBuilder metadata = new StringBuilder();
+		metadata.append("\n[Author=");
+		metadata.append(author);
+		metadata.append(" Created=");
+		metadata.append(changedOn);
+		metadata.append("]\n");
+		return metadata.toString();
 	}
 }
